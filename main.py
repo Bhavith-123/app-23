fcc1f30b71a601b5332ae819cf424e0611b52b30
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-18 02:03:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')  
-def home ():  
+def home():  
     return redirect(url_for("appform"))
  
 @app.route('/appform')  
@@ -14,11 +14,16 @@ def appform():
 def verify():  
     if request.method == 'POST' and request.form['password'] == 'Bhavith@123':  
           return redirect(url_for("success"))  
-          return redirect(url_for("appform'"))
+        return redirect(url_for("failure'"))
  
 @app.route('/success')  
 def success():  
     return 'logged in successfully'
+
+app.route('/failure')  
+def failure():  
+    return "<html><body><h2 style="text-align:center;color:red;'><b>Your account is Unauthorised!!</b></h2></body></html>"
+
 
 @app.errorhandler(404)
 def page_not_found(error):
