27be4957a294bf99eb9f25e614cdd56e0696e891
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-18 01:03:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('app'))
+    return render_template('index.html')
 
 @app.route('/app')
 def home_page():
