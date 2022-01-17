1dad238e5e87bf4f6037586881850ea1b5721ad0
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-18 04:48:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,12 @@ def login():
 def welcome():  
     return render_template('index.html')
 
-@app.route('/verify', methods = ['GET', 'POST'])
+@app.route('/verify', method = ['POST'])
 def verify():
-        if request.method == 'POST':
-            if request.form[username] == 'admin' and request.form[password] == 'admin@123':
-                return redirect(url_for('welcome'))
-            else:
-                return redirect(url_for('login'))
+      if request.form[username] == 'admin' and request.form[password] == 'admin@123':
+           return redirect(url_for('welcome'))
+      else:
+           return redirect(url_for('login'))
 
 @app.errorhandler(404)
 def page_not_found(error):
