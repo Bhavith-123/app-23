d6a641bbcf793d774a6c6b59c4e5d86bd1f678cd
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-18 04:09:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def login():
     return render_template("login.html")
 
 @app.route('/home')  
-def login():  
+def home():  
     return render_template("index.html")
 
 @app.route("/verify", methods=["GET", "POST"])
