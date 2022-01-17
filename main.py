83e710d7e9b68d650864bc625f8e5573b2f1ef1c
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-17 23:04:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
