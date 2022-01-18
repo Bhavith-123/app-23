507e24283d63ce9ac99c2b3d0246a19f3d358316
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-18 13:13:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,29 @@ app = Flask(__name__)
  
 @app.route('/')  
 def index():  
-    return redirect(url_for('defaultuserlogin'))
+    return redirect(url_for('login'))
  
-@app.route('/defaultuserlogin')  
+@app.route('/login')  
 def defaultuserlogin():  
     return render_template('login.html')
 
 @app.route('/welcome')  
 def welcome():  
     return render_template('index.html')
+
+@app.route('/verify', methods = ['POST'])
+def validate():
+    username = request.form[username]
+    password = request.form[password]
+    
+    if username == 'admin' and password == 'admin@123':
+        return redirect(url_for('success'))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/success')
+def success ():
+     return 'Welcome!!! You are Logged In Successfully'
 
 @app.errorhandler(404)
 def page_not_found(error):
